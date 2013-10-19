bcf6284d3a558810ecea7066d6b03ae4ecc43c27
Jeff Carouth <jcarouth@gmail.com>
2013-10-18 20:09:33 -0500

    Fix autoload issues in unit tests

--- site/tests/src/MentorApp/UserServiceTest.php
+++ site/tests/src/MentorApp/UserServiceTest.php
@@ -4,7 +4,7 @@ class UserServiceTest extends \PHPUnit_Framework_TestCase
 {
     public function testTrueTest()
     {
-        new UserService(new \PDO);
+        new UserService(new \PDOTestHelper());
         $this->assertTrue(true);
     }
 }
